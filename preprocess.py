9556ee8b64102897a992fe7b5dea2bd1616c5eb6
katerinamant <katerinamantaraki@gmail.com>
2023-12-29 21:09:17 +0200

    small changes

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 # from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -15,24 +16,29 @@ class Preprocess:
     M: int = 0
 
     def __init__(self, vocabulary_path: str) -> None:
-        self.vectorizer = CountVectorizer(binary=True, min_df=100)
+        self.vocabulary = self.extract_vocabulary(vocabulary_path)
+        self.vectorizer = CountVectorizer(vocabulary=self.vocabulary, binary=True)
+
+    def extract_vocabulary(self, vocabulary_path: str) -> dict[str, int]:
+        '''
+        Parses dataset vocabulary and creates dictionary mapping
+        each word to its index in a review's attribute vector.
+        '''
+        vocabulary = pd.read_fwf(vocabulary_path, skiprows=self.N, skipfooter=self.K, names=['vocab'])
+        self.M = vocabulary.size
+        return dict(zip(vocabulary.vocab, range(self.M)))
 
     def preprocess_reviews(self):
-        '''
-        Iterates through positive / negative reviews directory,
-        processes every review and returns
-        a x_train and a y_train array.
-        '''
         (x_train_imdb, y_train_imdb), (x_test_imdb, y_test_imdb) = tf.keras.datasets.imdb.load_data()
 
-        word_index = tf.keras.datasets.imdb.get_word_index()
+        word_index = tf.keras.datasets.imdb.get_word_index() # dict {index : word}
         index2word = dict((i + 3, word) for (word, i) in word_index.items())
         index2word[0] = '[pad]'
         index2word[1] = '[bos]'
         index2word[2] = '[oov]'
-        x_train_imdb = np.array([' '.join([index2word[idx] for idx in text]) for text in x_train_imdb])
-        x_test_imdb = np.array([' '.join([index2word[idx] for idx in text]) for text in x_test_imdb])
-        x_train_imdb_binary = self.vectorizer.fit_transform(x_train_imdb)
+        x_train_imdb = np.array([' '.join([index2word[idx] for idx in text]) for text in x_train_imdb]) # get string from indeces
+        x_test_imdb = np.array([' '.join([index2word[idx] for idx in text]) for text in x_test_imdb]) # get string from indeces
+        x_train_imdb_binary = self.vectorizer.transform(x_train_imdb)
         x_test_imdb_binary = self.vectorizer.transform(x_test_imdb)
 
         return (x_train_imdb_binary.toarray(), y_train_imdb, x_test_imdb_binary.toarray(), y_test_imdb)
